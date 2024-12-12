604903e58381eb0740ad0290409d4d1d647d6718
Aakash <sidh0148@algonquinlive.com>
2024-12-12 02:35:55 -0500

    new

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -40,7 +40,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #eaed2bf0;
+  background-color: #3044def0;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
